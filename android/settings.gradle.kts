eda97287da913c0791aa61cbf242390b0cdffa4f
shailesh43 <satheshailesh43@gmail.com>
2026-06-03 17:06:15 +0530

    add: Azure redirectURI for android

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,26 +1,45 @@
-pluginManagement {
-    val flutterSdkPath =
-        run {
-            val properties = java.util.Properties()
-            file("local.properties").inputStream().use { properties.load(it) }
-            val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-            flutterSdkPath
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.tatapower.greengears"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17" // Correct Kotlin DSL syntax
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID
+        applicationId = "com.tatapower.holidayhomes"
+        minSdk = 29
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+        manifestPlaceholders["appAuthRedirectScheme"] = "msauth"
+    }
+
+    buildTypes {
+        release {
+            // Signing with debug keys for now
+            signingConfig = signingConfigs.getByName("debug")
         }
-
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
-
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
+        debug {
+            // Optional: ensure debug build also has placeholder
+            manifestPlaceholders["appAuthRedirectScheme"] = "msauth"
+        }
     }
 }
 
-plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "9.0.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
+flutter {
+    source = "../.."
 }
-
-include(":app")
