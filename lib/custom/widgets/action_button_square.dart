8af81995be63ec52fda8f6a9b1722bd0bff774d2
shailesh43 <satheshailesh43@gmail.com>
2026-06-16 11:50:24 +0530

    update: action buttons for navigators

--- lib/custom/widgets/action_button_square.dart
+++ lib/custom/widgets/action_button_square.dart
@@ -7,6 +7,7 @@ class ActionButtonSquare extends StatelessWidget {
   final Color? iconBgColor;
   final Color? iconColor;
   final Color? labelColor;
+  final bool enabled;
 
   const ActionButtonSquare({
     super.key,
@@ -16,40 +17,54 @@ class ActionButtonSquare extends StatelessWidget {
     this.iconBgColor,
     this.iconColor,
     this.labelColor,
+    this.enabled = true,
   });
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            width: 64,
-            height: 64,
-            decoration: BoxDecoration(
-              color: iconBgColor ?? const Color.fromRGBO(232, 240, 254, 1),
-              borderRadius: BorderRadius.circular(16),
+    final resolvedIconBg = enabled
+        ? (iconBgColor ?? const Color.fromRGBO(232, 240, 254, 1))
+        : Colors.grey.shade200;
+    final resolvedIconColor = enabled
+        ? (iconColor ?? const Color.fromRGBO(0, 100, 200, 0.75))
+        : Colors.grey.shade400;
+    final resolvedLabelColor = enabled
+        ? (labelColor ?? const Color.fromRGBO(5, 25, 75, 1))
+        : Colors.grey.shade400;
+
+    return Opacity(
+      opacity: enabled ? 1.0 : 0.5,
+      child: GestureDetector(
+        onTap: enabled ? onTap : null,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: 64,
+              height: 64,
+              decoration: BoxDecoration(
+                color: resolvedIconBg,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Icon(
+                icon,
+                size: 32,
+                color: resolvedIconColor,
+              ),
             ),
-            child: Icon(
-              icon,
-              size: 32,
-              color: iconColor ?? const Color.fromRGBO(0, 100, 200, 0.75),
+            const SizedBox(height: 8),
+            Text(
+              label,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w500,
+                letterSpacing: -0.2,
+                color: resolvedLabelColor,
+              ),
             ),
-          ),
-          const SizedBox(height: 8),
-          Text(
-            label,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w500,
-              letterSpacing: -0.2,
-              color: labelColor ?? const Color.fromRGBO(5, 25, 75, 1),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/custom/widgets/action_card_wide.dart
+++ lib/custom/widgets/action_card_wide.dart
@@ -7,6 +7,7 @@ class ActionCardWide extends StatelessWidget {
   final VoidCallback onTap;
   final Color? iconBgColor;
   final Color? iconColor;
+  final bool enabled;
 
   const ActionCardWide({
     super.key,
@@ -16,59 +17,70 @@ class ActionCardWide extends StatelessWidget {
     required this.onTap,
     this.iconBgColor,
     this.iconColor,
+    this.enabled = true,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.fromLTRB(4, 8, 4, 8),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              color: iconBgColor ?? Colors.grey.shade200,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Icon(
-              icon,
-              size: 28,
-              color: iconColor ?? Colors.grey.shade600,
-            ),
-          ),
-          const SizedBox(width: 16),
-          Expanded(
-            child: InkWell(
-              onTap: onTap,
-              borderRadius: BorderRadius.circular(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title,
-                    style: const TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600,
-                      color: Color.fromRGBO(0, 0, 0, 0.80),
-                    ),
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    subtitle,
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.grey[500],
-                    ),
-                  ),
-                ],
+    final resolvedIconBg =
+    enabled ? (iconBgColor ?? Colors.grey.shade200) : Colors.grey.shade100;
+    final resolvedIconColor =
+    enabled ? (iconColor ?? Colors.grey.shade600) : Colors.grey.shade400;
+
+    return Opacity(
+      opacity: enabled ? 1.0 : 0.5,
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.fromLTRB(4, 8, 4, 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: resolvedIconBg,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Icon(
+                icon,
+                size: 28,
+                color: resolvedIconColor,
               ),
             ),
-          ),
-        ],
+            const SizedBox(width: 16),
+            Expanded(
+              child: InkWell(
+                onTap: enabled ? onTap : null,
+                borderRadius: BorderRadius.circular(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                        color: enabled
+                            ? const Color.fromRGBO(0, 0, 0, 0.80)
+                            : Colors.grey.shade400,
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      subtitle,
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: enabled ? Colors.grey[500] : Colors.grey[350],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
